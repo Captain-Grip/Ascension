b5d60b2bc15adfc13cee03773e08f3e773e41c54
Captain-Grip <jrumenieras@gmail.com>
2026-07-17 10:55:07 +0200

    Add BootScene class for game initialization

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,1 +1,20 @@
+export default class BootScene extends Phaser.Scene {
 
+    constructor() {
+        super("BootScene");
+    }
+
+    preload() {
+
+        // Ici nous chargerons plus tard les sprites,
+        // les sons, les maps, etc.
+
+    }
+
+    create() {
+
+        this.scene.start("MenuScene");
+
+    }
+
+}
